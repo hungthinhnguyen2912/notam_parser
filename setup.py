83abf2d2fecb43b2f248061cd6988e03724d4f2a
HungThinh <hungthinhnht2912@gmail.com>
2025-05-29 16:49:13 +0700

    fix format out put and add some def

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from importlib_metadata import entry_points
 from setuptools import setup,find_packages
 
 setup(
     name='parser_notam_package',
-    version='0.1',
+    version='0.2',
+    packages=find_packages(),
     entry_points= {
 
     }
